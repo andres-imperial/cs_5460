a9ec0fbf55f572ee0edba24b97ac3de8ac7fe612
marketplaceofideas <42588612+marketplaceofideas@users.noreply.github.com>
2019-11-04 12:54:39 -0700

    Small script for p/w def and minor fixes

--- script.js
+++ script.js
@@ -1,36 +1,16 @@
-// Initial data for exchange rates
-var exchangeRates = {
-  "disclaimer": "Usage subject to terms: https://openexchangerates.org/terms",
-  "license": "https://openexchangerates.org/license",
-  "timestamp": 1534107604,
-  "base": "USD",
-  "rates": {
-    "BTC": 0.000157753542,
-    "CAD": 1.316853,
-    "EUR": 0.879353,
-    "JPY": 110.46550427,
-    "USD": 1,
+// class for user info in task 2
+var userInfo = {
+  "fname": "",
+  "address": {
+    "street": "",
+	"apt": "",
   }
+  // ...
 };
 
-var $currSelect = $("#toCurrency");
-$(document).ready(function() {
-    $currSelect.html("<option value='' disabled selected>Select currency</option>");
-    for (var i in exchangeRates.rates)
-        $currSelect.append("<option value='" + i + "'>" + allCurrencies[i] + " (" + i + ")</option>");
-});
-
-$currSelect.change(function() {
-    $("#resultCurrency").val((exchangeRates.rates[$(this).val()] * $("#usdInput").val()).toFixed(2));
-    $("#resultLabel").text(allCurrencies[$(this).val()] + " (" + $(this).val() + ")");
-});
-
-var $updateRates = $("#updateRates");
-$updateRates.click(function() {
-    exchangeRates = JSON.parse($("#exchangeRates").text());
-    $currSelect.html("<option value='' disabled selected>Select currency</option>");
-    for (var i in exchangeRates.rates)
-        $currSelect.append("<option value='" + i + "'>" + allCurrencies[i] + " (" + i + ")</option>");
-    $("#resultLabel").text("To Currency ():");
-    $("#resultCurrency").val("---.--");
-});
+//var $form = $("#form");
+var password;
+function definePassword() {
+	password = $("#form ul li input#password").val();
+	console.log(password);
+}
