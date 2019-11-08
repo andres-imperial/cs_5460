87bfb1525127b9352fd4b1ac458c961e2960f0c3
Stephanie G <sswenson.anne@gmail.com>
2019-11-08 10:35:50 -0700

    added multiple users, fixed the timeout on module, and added readme

--- securityassn4/src/App.js
+++ securityassn4/src/App.js
@@ -27,7 +27,7 @@ const App = () => {
   }, [badLoginAttempts]);
 
   useEffect(() => {
-    let timer1 = setTimeout(() => setShowModal(false), (2 ** timeoutExponent) * 1000)
+    let timer1 = setTimeout(() => setShowModal(false), (2 ** timeoutExponent) * 60000)
 
     return () => clearTimeout(timer1)
   }, [timeoutExponent]);
@@ -44,7 +44,8 @@ const App = () => {
       hashedPasswordArray.forEach(item => {
         hashedPassword += item.toString();
       });
-      if (hashedPassword === localStorage.getItem('password')) {
+      let username = values.userName;
+      if (hashedPassword === localStorage.getItem(username)) {
         setLoginMessage('Successful Login');
         setBadLoginAttempts(0);
       } else {
